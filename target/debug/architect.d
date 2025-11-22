c8fb7ac20fee8b77496e6f1801866cfffdba07d7
Iron Man <your_github_email@example.com>
2025-11-22 18:50:51 +0300

    Add target directory

---
+++ target/debug/architect.d
@@ -0,0 +1,1 @@
+/data/data/com.termux/files/home/architect/target/debug/architect: /data/data/com.termux/files/home/architect/src/main.rs
---
+++ target/debug/deps/architect-239dd96379d0f08a.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/architect/target/debug/deps/architect-239dd96379d0f08a.d: src/main.rs
+
+/data/data/com.termux/files/home/architect/target/debug/deps/architect-239dd96379d0f08a: src/main.rs
+
+src/main.rs:
